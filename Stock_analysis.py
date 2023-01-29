1f7ebe8d27ff1a1962b688197119b98c33976974
elChoorizo <60880896+elChoorizo@users.noreply.github.com>
2023-01-29 11:37:32 +0000

    Add files via upload

---
+++ Stock_analysis.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 10:25:54 2022
+
+@author: Thomas Chantoin
+"""
+
+import numpy as np
+import pandas_datareader as pdr
+import datetime as dt
+import pandas as pd
+import matplotlib.pyplot as plt
+start = dt.datetime(2019, 1, 1)
+stock_name = "AAPL"
+data = pdr.get_data_yahoo(stock_name, start)
+data.head()
+high_low = data['High'] - data['Low']
+high_cp = np.abs(data['High'] - data['Close'].shift())
+low_cp = np.abs(data['Low'] - data['Close'].shift())
+df = pd.concat([high_low, high_cp, low_cp], axis=1)
+true_range = np.max(df, axis=1)
+average_true_range = true_range.rolling(25).mean()
+#moving_average_25 =  data['Close'].rolling(50).mean()
+
+
+
+#fig, ax = plt.subplots()
+#average_true_range.plot(ax=ax)
+#ax2 = data['Close'].plot(ax=ax, secondary_y=True, alpha=.5, color ='red')
+#ax.set_ylabel("ATR")
+#ax2.set_ylabel("Price")
+
+fig, ax = plt.subplots(figsize=(10, 6), dpi = 400)
+ax.set(title = stock_name)
+ax2 = data['Close'].plot(ax=ax, secondary_y=False, alpha=.5, color ='red')
+ax2.set_ylabel("Price")
+ax3 = average_true_range.plot(ax=ax, secondary_y=True, alpha=.5, color ='blue')
+ax3.set_ylabel("ATR")
+fig, ax = plt.subplots(figsize=(10, 6), dpi = 400)
+ax.set(title = stock_name)
+ax2 = data['Close'].plot(ax=ax, secondary_y=False, alpha=.5, color ='red')
+data['MA20'] = data['Close'].rolling(100).mean()
+#data.tail()
+data['EMA21'] = data['Close'].ewm(span=105, adjust=False).mean()
+#data.tail()
+#fig, ax = plt.subplots()
+data[['MA20', 'EMA21']].loc['2019-01-01':].plot(ax=ax)
